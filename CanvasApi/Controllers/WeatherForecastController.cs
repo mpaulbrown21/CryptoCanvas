d244d635889356879b0e86050601ae659b880e0f
matthew brown <mpbhome@hotmail.com>
2021-06-05 15:03:34 -0600

    Created api endpoint that returns an image.

--- CanvasApi/Controllers/WeatherForecastController.cs
+++ CanvasApi/Controllers/WeatherForecastController.cs
@@ -2,8 +2,9 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CanvasApi.Controllers
 {
@@ -24,16 +25,37 @@ namespace CanvasApi.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult GetDocumentBytes()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var bitmap = GetCanvas();
+
+            var byteArray = ImageToByte2(bitmap);
+
+            return new FileContentResult(byteArray, "image/png");
+        }
+
+        private Bitmap GetCanvas()
+        {
+            var bitmap = new Bitmap(640, 480);
+
+            for (var x = 0; x < bitmap.Width; x++)
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    bitmap.SetPixel(x, y, Color.BlueViolet);
+                }
+            }
+
+            return bitmap;
+        }
+
+        private static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
         }
     }
 }
